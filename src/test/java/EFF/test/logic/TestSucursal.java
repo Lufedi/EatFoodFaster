dfb11766e1848f5ac2138dd9f3c162351895a741
FabianAlvarez07 <fandresalvarez@hotmail.com>
2015-03-09 14:31:13 -0500

    dependencias de spring en el pom

--- src/test/java/EFF/test/logic/TestSucursal.java
+++ src/test/java/EFF/test/logic/TestSucursal.java
@@ -5,14 +5,27 @@
  */
 package EFF.test.logic;
 
+import edu.eci.cosw.proyecto_eff.logic.LogicaSucursal;
+import edu.eci.cosw.proyecto_eff.model.Cliente;
+import edu.eci.cosw.proyecto_eff.model.Pedido;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  *
  * @author FabianAndres
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContextH2.xml"})
+
 public class TestSucursal {
+    
+    @Autowired
+    LogicaSucursal ls;
     
     @Test
     public void testNotificarCliente(){
@@ -21,6 +34,10 @@ public class TestSucursal {
     
     @Test
     public void testRecibirNotificacion(){
+        
+        Pedido p= new Pedido(new Cliente("fan@hot.com", "123456", "Fabian", "Alvarez", "310582"),
+                false, false, "Pedido sin todavia enviarse a la sucursal");
+        
         assertEquals(true,true);
     }
     
