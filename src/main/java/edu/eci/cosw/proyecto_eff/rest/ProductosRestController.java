c39fadd51aa396c8c73ed25bd1638142425aa444
luisfediaz <pipexir@gmail.com>
2015-03-10 23:32:19 -0500

    Avance testRest y agregando reportes al pom

--- src/main/java/edu/eci/cosw/proyecto_eff/rest/ProductosRestController.java
+++ src/main/java/edu/eci/cosw/proyecto_eff/rest/ProductosRestController.java
@@ -11,6 +11,8 @@ import edu.eci.cosw.proyecto_eff.model.Pedido;
 import edu.eci.cosw.proyecto_eff.model.Producto;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -39,7 +41,7 @@ public class ProductosRestController {
         return lp.consultarProductosPorNombre(nombre);
     }
     
-    @RequestMapping(value="/{id}",method = RequestMethod.GET)        
+    @RequestMapping(value="/id/{id}",method = RequestMethod.GET)        
     public Pedido consultarPedidosPorId(@PathVariable int id) throws ResourceNotFoundException  {       
         return lp.getProducto(id);
     }
@@ -52,5 +54,8 @@ public class ProductosRestController {
        return lp.consultarProductosPorNombre(centrocomercial  , ciudad  ,   nombre);
     }
     
-    
+  /*@RequestMapping(value="/echo/{input}",method = RequestMethod.GET)        
+    public ResponseEntity<?> consultaX(@PathVariable String input) {       
+        return new ResponseEntity<>("REST API working. Echo:"+input,HttpStatus.ACCEPTED);
+    }*/
 }
