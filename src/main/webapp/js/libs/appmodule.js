8ac5bccaa0ff604c36cac97d1aa01f0eafa72505
FabianAlvarez07 <fandresalvarez@hotmail.com>
2015-04-21 21:26:04 -0500

    Estado de los pedidos

--- src/main/webapp/js/libs/appmodule.js
+++ src/main/webapp/js/libs/appmodule.js
@@ -9,7 +9,37 @@
                     templateUrl: 'estado.html'
                 });
     });
-})();
+    
+    app.controller('controlpedidos',
+                function($scope,$http){
+                    
+                    $scope.lista=[1,2,3,4];
+                    this.consultar=function(){
+                     
+                        
+                        $http.get('rest/pedidos/').
+                        success(function(data, status, headers, config) {
+                          // this callback will be called asynchronously
+                          // when the response is available
+                          $scope.lista=data;
+                            
+                               
+                        }).
+                        error(function(data, status, headers, config) {
+                          // called asynchronously if an error occurs
+                          // or server returns response with an error status.
+                          alert("error!");
+                        });
+                        
+                        
+                        
+                    };
+                }
+                                   
+    );
+    
+})
+();
 
 
 
