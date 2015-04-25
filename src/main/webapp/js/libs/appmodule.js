6e39115914b9285e5b96402d5b0bf59c11d51839
FabianAlvarez07 <fandresalvarez@hotmail.com>
2015-04-24 19:11:43 -0500

    Pedidos sin notificar por parte de la sucursal

--- src/main/webapp/js/libs/appmodule.js
+++ src/main/webapp/js/libs/appmodule.js
@@ -5,6 +5,10 @@
         $routeProvider
 
                 // route for the about page
+                .when('/pedidosSinNotificar', {
+                    templateUrl: 'pedidosSinNotificar.html'
+                })
+                
                 .when('/estado', {
                     templateUrl: 'estado.html'
                 });
@@ -14,6 +18,7 @@
                 function($scope,$http){
                     
                     $scope.lista=[1,2,3,4];
+                    $scope.error;
                     this.consultar=function(){
                      
                         
@@ -29,6 +34,10 @@
                           // called asynchronously if an error occurs
                           // or server returns response with an error status.
                           alert("error!");
+                          
+                          alert(status);
+                          error=data;
+                          alert(error);
                         });
                         
                         
@@ -37,6 +46,99 @@
                 }
                                    
     );
+    
+    app.controller('controlpedidosSinNotificar',
+                function($scope,$http){
+                    
+                    //$scope.lista=[1,2,3,4];
+                    $scope.sucursal=0;
+                    
+                    $scope.lista=[{idPedidos:0,
+                      enviadoAsucursal:false},{idPedidos:1,
+                      enviadoAsucursal:false}];
+                      
+                    
+                    $scope.idPedidoEsco;
+                    $scope.options = [
+                        { label: 'onee', value: 1 },
+                        { label: 'twoee', value: 2 }
+                      ];
+                     $scope.correctlySelected = $scope.options[0];
+                    
+                    this.consultarSucursales=function(){
+                        //Arreglar URL
+                        $http.get('rest/pedidos/sucursal/sinNotificar/'+$scope.sucursal).
+                        //$http.get('https://eatfoodfaster.herokuapp.com/rest/pedidos/').
+                        success(function(data, status, headers, config) {
+                          // this callback will be called asynchronously
+                          // when the response is available
+                          $scope.options=data;
+                            
+                               
+                        }).
+                        error(function(data, status, headers, config) {
+                          // called asynchronously if an error occurs
+                          // or server returns response with an error status.
+                          alert("error!");
+                          
+                          alert(status);
+                          
+                        });
+                        
+                    };
+                    
+                    
+                    this.consultar=function(){
+                     
+                        
+                        $http.get('rest/pedidos/sucursal/sinNotificar/'+$scope.sucursal).
+                        //$http.get('https://eatfoodfaster.herokuapp.com/rest/pedidos/').
+                        success(function(data, status, headers, config) {
+                          // this callback will be called asynchronously
+                          // when the response is available
+                          $scope.lista=data;
+                            
+                               
+                        }).
+                        error(function(data, status, headers, config) {
+                          // called asynchronously if an error occurs
+                          // or server returns response with an error status.
+                          alert("error!");
+                          
+                          alert(status);
+                          
+                        });
+                        
+                        
+                        
+                    };
+                    
+                    $scope.estado={estado:"Pedido recibido en sucursal"};
+                    
+                    this.enviarPedido=function(idPedido){
+                        $scope.idPedidoEsco=idPedido;
+                        alert($scope.idPedidoEsco);
+                        $http.put('rest/notificacion/pedido/sucursal/'+$scope.idPedidoEsco,$scope.estado).
+                        
+                        success(function(data, status, headers, config) {
+                          // this callback will be called asynchronously
+                          // when the response is available
+                                alert("Success!");
+                            
+                               
+                        }).
+                        error(function(data, status, headers, config) {
+                          // called asynchronously if an error occurs
+                          // or server returns response with an error status.
+                          alert("error!");
+                          
+                        });
+                    };
+                }
+                                   
+    );
+    
+    
     
 })
 ();
