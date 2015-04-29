5df25f0e4f3e65aa75941256aa4e9c1c2c1a8516
luisfediaz <pipexir@gmail.com>
2015-04-29 02:06:42 -0500

    Agregando angular al template , creacion de la plantilla  y consular_productos.html , correcto funcionamiento dl ng-view

---
+++ src/main/webapp/web/js/angularjs/appmodule.js
@@ -0,0 +1,73 @@
+(function () {
+    var app = angular.module('eff', ['ngRoute']);
+
+    app.controller('ctrl1' ,  
+        function($scope ,  $http){
+            this.producto = {
+                nombre:'camisa',
+                idproducto:12,
+                precio:13
+            }
+            
+            
+            this.registrar = function (api) {
+               // alert("registrando un producto");
+            
+            $http.post('rest/products/' + api, this.producto)
+                    .success(function (data, status, headers, config) {
+                        alert('success');
+                     })
+                    .error(function (data, status, headers, config) {
+                        alert('error!') });
+            };
+        }
+    );
+    
+    app.controller('catalogoClr' ,
+        function($scope , $http){
+            $scope.productos = [];
+            
+            this.actualizar =  function(api){
+                //alert('rest/products/' + api + '/');
+                 $http.get('rest/products/' + api)
+                    .success(function (data, status, headers, config) {
+                     //  alert('success'  +  data);
+                        $scope.productos =  data;//adf
+                     })
+                    .error(function (data, status, headers, config) {
+                        alert('error!') });
+            
+            };
+            
+        }
+    );
+    app.config(function ($routeProvider) {
+        $routeProvider
+
+                // route for the about page
+                .when('/consultarproductos', {
+                    templateUrl: 'eff_consultar_productos.html'
+
+                })
+
+                // route for the contact page
+                .when('/list', {
+                    templateUrl: 'catalogo.html'
+
+                })
+                
+                .when('/autor' , {
+                    templateUrl : 'autor.html'
+                });
+                       
+
+    });
+
+
+
+})();
+
+
+
+
+
