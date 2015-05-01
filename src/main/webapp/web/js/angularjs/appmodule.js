19936f4253194c947cfde42679a64fa6d91386ce
Lufedi <pipexir@gmail.com>
2015-05-01 18:41:04 -0500

    ng-click error

--- src/main/webapp/web/js/angularjs/appmodule.js
+++ src/main/webapp/web/js/angularjs/appmodule.js
@@ -41,13 +41,37 @@
             
         }
     );
+
+    app.controller('ProductoCrtl',
+        function($scope , $http){
+            $scope.productos = [];
+            $scope.txt = 'controladorfuncionando';
+
+            this.buscar =  function(){
+                alert('buscar');
+            };
+
+            this.buscarProductos =  function(nombre){
+                alert('buscando  ' + nombre);
+              $http.get('/productos/' + nombre)
+                  .success(function (data, status, headers, config) {
+                      //  alert('success'  +  data);
+                      $scope.productos =  data;//adf
+                  })
+                  .error(function (data, status, headers, config) {
+                      alert('error  consultado productos!') });
+
+            };
+
+        }
+    );
     app.config(function ($routeProvider) {
         $routeProvider
 
                 // route for the about page
                 .when('/consultarproductos', {
-                    templateUrl: 'eff_consultar_productos.html'
-
+                    templateUrl: 'eff_consultar_productos.html',
+                    controller:  'ProductoCrtl'
                 })
 
                 // route for the contact page
